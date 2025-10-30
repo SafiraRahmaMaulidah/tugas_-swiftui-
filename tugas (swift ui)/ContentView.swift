83cf007ead7553b56a38381d0be05bb03938b76c
SafiraRahmaMaulidah <111202214631@mhs.dinus.ac.id>
2025-10-30 12:26:29 +0700

    Initial Commit

---
+++ tugas (swift ui)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tugas (swift ui)
+//
+//  Created by MacBook Pro M1 on 30/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tugas (swift ui)/tugas__swift_ui_App.swift
@@ -0,0 +1,17 @@
+//
+//  tugas__swift_ui_App.swift
+//  tugas (swift ui)
+//
+//  Created by MacBook Pro M1 on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct tugas__swift_ui_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
